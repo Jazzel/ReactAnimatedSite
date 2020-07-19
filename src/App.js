0d6dfb0c2eb0a8e99e178848a0068f2a2aa3f581
Jazzel Mehmood <jazzelmehmood6@gmail.com>
2020-07-19 21:56:17 +0500

    Almost done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { useRef} from "react";
+import React, { useRef } from "react";
 import "./App.css";
+import {
+  Facebook,
+  Twitter,
+  Whatsapp,
+  Instagram,
+  Close,
+  Menu,
+} from "mdi-material-ui";
+import { Grid, Typography } from "@material-ui/core";
 
 function App() {
   const triggerContainer = useRef(null);
+  const icon = useRef(null);
 
   function openOverlay() {
     var keyFrames = [{ height: "0vh" }, { height: "300vh" }];
     triggerContainer.current.style.display = "flex";
+    icon.current.style.display = "none";
 
     var animation = triggerContainer.current.animate(keyFrames, {
       duration: 500,
@@ -58,6 +69,7 @@ function App() {
     }, 470);
     setTimeout(() => {
       triggerContainer.current.style.display = "none";
+      icon.current.style.display = "block";
     }, 495);
   }
 
@@ -66,32 +78,154 @@ function App() {
       <div className="container">
         <section className="section">
           <div className="main-navigation">
-            <h1 onClick={openOverlay} style={{textAlign:"center"}} >Click here</h1>
+            <h1
+              onClick={openOverlay}
+              style={{
+                textAlign: "center",
+                fontFamily: "Geomanist-Bold",
+                cursor: "pointer",
+              }}
+            >
+              JAZZHASH
+            </h1>
+            <h1
+              onClick={openOverlay}
+              ref={icon}
+              style={{
+                position: "absolute",
+                cursor: "pointer",
+                top: "0px",
+                right: "10px",
+              }}
+            >
+              <Menu />
+            </h1>
+          </div>
+          <div className="sub-content">
+            <br />
+            <br />
+            <Typography
+              variant="h3"
+              style={{
+                textAlign: "center",
+                fontFamily: "Geomanist-Bold",
+                cursor: "pointer",
+              }}
+            >
+              Welcome to React Animation
+            </Typography>
+            <br />
+            <Typography variant="body1">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </Typography>
           </div>
 
           <div className="trigger-container" ref={triggerContainer}>
             <div className="inner-container">
-              <div className="navigation"></div>
+              <div className="navigation">
+                <ul>
+                  <li>work</li>
+                  <li>approach</li>
+                  <li>create</li>
+                  <li>innovate</li>
+                </ul>
+              </div>
+              <div style={{ width: "100%" }}>
+                <Grid container style={{ color: "white" }} spacing={3}>
+                  <Grid xs={12} sm={4} md={4} lg={4} item>
+                    <Typography
+                      variant="body1"
+                      style={{
+                        textTransform: "uppercase",
+                        fontFamily: "Geomanist-Bold",
+                      }}
+                    >
+                      Facebook
+                    </Typography>
+                    <Typography variant="body1">
+                      <a
+                        style={{
+                          fontFamily: "Intro-Light",
+                          color: "white",
+                        }}
+                        href="https://www.facebook.com/jazzel.mehmood/"
+                      >
+                        Muhammad Jazzel Mehmood
+                      </a>
+                    </Typography>
+                  </Grid>
+                  <Grid xs={12} sm={4} md={4} lg={4} item>
+                    <Typography
+                      variant="body1"
+                      style={{
+                        textTransform: "uppercase",
+                        fontFamily: "Geomanist-Bold",
+                      }}
+                    >
+                      Github
+                    </Typography>
+                    <Typography variant="body1">
+                      <a
+                        style={{
+                          fontFamily: "Intro-Light",
+                          color: "white",
+                        }}
+                        href="https://github.com/Jazz-hash"
+                      >
+                        https://github.com/Jazz-hash
+                      </a>
+                    </Typography>
+                  </Grid>
+                  <Grid xs={12} sm={4} md={4} lg={4} item>
+                    <Typography
+                      variant="body1"
+                      style={{
+                        textTransform: "uppercase",
+                        fontFamily: "Geomanist-Bold",
+                      }}
+                    >
+                      Gmail
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      style={{
+                        fontFamily: "Intro-Light",
+                      }}
+                    >
+                      jazzelmehmood4@gmail.com
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </div>
               <hr />
               <div className="buttons-group">
                 <button className="social-buttons" type="button">
-                  F
+                  <Twitter className="icons" />
                 </button>
                 <button className="social-buttons" type="button">
-                  F
+                  <Facebook className="icons" />
                 </button>
                 <button
                   className="trigger-close"
                   onClick={closeOverlay}
                   type="button"
                 >
-                  Close Overlay
+                  <Close className="main-icon" />
                 </button>
                 <button className="social-buttons" type="button">
-                  F
+                  <Whatsapp className="icons" />
                 </button>
                 <button className="social-buttons" type="button">
-                  F
+                  <Instagram className="icons" />
                 </button>
               </div>
             </div>
