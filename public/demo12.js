b15062abe3efff2ba99043d745511cd89c5496c3
Jazzel Mehmood <jazzelmehmood6@gmail.com>
2020-07-19 17:52:05 +0500

    Animation done

--- public/demo12.js
+++ public/demo12.js
@@ -1,58 +1,58 @@
-(function() {
-	var triggerBttn = document.getElementById( 'trigger-overlay' ),
-		overlay = document.querySelector( 'div.overlay' ),
-		closeBttn = overlay.querySelector( 'button.overlay-close' );
-		transEndEventNames = {
-			'WebkitTransition': 'webkitTransitionEnd',
-			'MozTransition': 'transitionend',
-			'OTransition': 'oTransitionEnd',
-			'msTransition': 'MSTransitionEnd',
-			'transition': 'transitionend'
-		},
-		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-		support = { transitions : Modernizr.csstransitions };
-		s = Snap( overlay.querySelector( 'svg' ) ), 
-		path = s.select( 'path' ),
-		steps = overlay.getAttribute( 'data-steps' ).split(';'),
-		stepsTotal = steps.length;
+import ("/snap.svg-min.js");
 
-		coneole.log(s)
+(function () {
+	const transEndEventNames = {
+		WebkitTransition: "webkitTransitionEnd",
+		MozTransition: "transitionend",
+		OTransition: "oTransitionEnd",
+		msTransition: "MSTransitionEnd",
+		transition: "transitionend",
+	  };
+  
+	  const transEndEventName = transEndEventNames["transition"];
+	  const support = { transitions: true };
+	  console.log(path.current);
+  
+	  // triggerBttn.addEventListener("click", toggleOverlay);
+	  // closeBttn.addEventListener("click", toggleOverlay);
+	});
 	function toggleOverlay() {
-		if( classie.has( overlay, 'open' ) ) {
-			var pos = stepsTotal-1;
-			classie.remove( overlay, 'open' );
-			classie.add( overlay, 'close' );
-			
-			var onEndTransitionFn = function( ev ) {
-					classie.remove( overlay, 'close' );
-				},
-				nextStep = function( pos ) {
-					pos--;
-					if( pos < 0 ) return;
-					path.animate( { 'path' : steps[pos] }, 60, mina.linear, function() { 
-						if( pos === 0 ) {
-							onEndTransitionFn();
-						}
-						nextStep(pos);
-					} );
-				};
-
-			nextStep(pos);
-		}
-		else if( !classie.has( overlay, 'close' ) ) {
-			var pos = 0;
-			classie.add( overlay, 'open' );
-			
-			var nextStep = function( pos ) {
-				pos++;
-				if( pos > stepsTotal - 1 ) return;
-				path.animate( { 'path' : steps[pos] }, 60, mina.linear, function() { nextStep(pos); } );
-			};
-
-			nextStep(pos);
-		}
-	}
-
-	triggerBttn.addEventListener( 'click', toggleOverlay );
-	closeBttn.addEventListener( 'click', toggleOverlay );
+	  const steps = "m 701.56545,809.01175 35.16718,0 0,19.68384 -35.16718,0 z;m 698.9986,728.03569 41.23353,0 -3.41953,77.8735 -34.98557,0 z;m 687.08153,513.78234 53.1506,0 C 738.0505,683.9161 737.86917,503.34193 737.27015,806 l -35.90067,0 c -7.82727,-276.34892 -2.06916,-72.79261 -14.28795,-292.21766 z;m 403.87105,257.94772 566.31246,2.93091 C 923.38284,513.78233 738.73561,372.23931 737.27015,806 l -35.90067,0 C 701.32034,404.49318 455.17312,480.07689 403.87105,257.94772 z;M 51.871052,165.94772 1362.1835,168.87863 C 1171.3828,653.78233 738.73561,372.23931 737.27015,806 l -35.90067,0 C 701.32034,404.49318 31.173122,513.78234 51.871052,165.94772 z;m 52,26 1364,4 c -12.8007,666.9037 -273.2644,483.78234 -322.7299,776 l -633.90062,0 C 359.32034,432.49318 -6.6979288,733.83462 52,26 z;m 0,0 1439.999975,0 0,805.99999 -1439.999975,0 z".split(
+		";"
+	  );
+	  const stepsTotal = steps.length;
+	  if (overlayClass === "open") {
+		var pos = stepsTotal - 1;
+		setOverlayClass("close");
+  
+		var nextStep = function (pos) {
+		  pos--;
+		  setOverlayClass("");
+  
+		  if (pos < 0) return;
+		  path.current.animate(
+			{ path: steps[pos] },
+			{ duration: 60000, easing: "linear" }
+		  );
+		};
+  
+		nextStep(pos);
+	  } else if (overlayClass !== "close") {
+		var pos = 0;
+		setOverlayClass("open");
+  
+		var nextStep = function (pos) {
+		  pos++;
+		  if (pos > stepsTotal - 1) return;
+		  path.current.animate(
+			{ path: steps[pos] },
+			{ duration: 60, easing: "linear" },
+			function () {
+			  nextStep(pos);
+			}
+		  );
+		};
+  
+		nextStep(pos);
+	  
 })();
--- src/App.js
+++ src/App.js
@@ -1,103 +1,102 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef} from "react";
 import "./App.css";
 
 function App() {
-  const triggerBttn = useRef(null);
-  const overlay = useRef(null);
-  const closeBttn = useRef(null);
-  const s = useRef(null);
-  const path = useRef(null);
-  const [overlayClass, setOverlayClass] = useState("close");
+  const triggerContainer = useRef(null);
 
-  useEffect(() => {
-    const transEndEventNames = {
-      WebkitTransition: "webkitTransitionEnd",
-      MozTransition: "transitionend",
-      OTransition: "oTransitionEnd",
-      msTransition: "MSTransitionEnd",
-      transition: "transitionend",
-    };
-    console.log(triggerBttn, overlay, closeBttn, s.current, path.current);
-    const steps = "m 701.56545,809.01175 35.16718,0 0,19.68384 -35.16718,0 z;m 698.9986,728.03569 41.23353,0 -3.41953,77.8735 -34.98557,0 z;m 687.08153,513.78234 53.1506,0 C 738.0505,683.9161 737.86917,503.34193 737.27015,806 l -35.90067,0 c -7.82727,-276.34892 -2.06916,-72.79261 -14.28795,-292.21766 z;m 403.87105,257.94772 566.31246,2.93091 C 923.38284,513.78233 738.73561,372.23931 737.27015,806 l -35.90067,0 C 701.32034,404.49318 455.17312,480.07689 403.87105,257.94772 z;M 51.871052,165.94772 1362.1835,168.87863 C 1171.3828,653.78233 738.73561,372.23931 737.27015,806 l -35.90067,0 C 701.32034,404.49318 31.173122,513.78234 51.871052,165.94772 z;m 52,26 1364,4 c -12.8007,666.9037 -273.2644,483.78234 -322.7299,776 l -633.90062,0 C 359.32034,432.49318 -6.6979288,733.83462 52,26 z;m 0,0 1439.999975,0 0,805.99999 -1439.999975,0 z".split(
-      ";"
-    );
-    const stepsTotal = steps.length;
+  function openOverlay() {
+    var keyFrames = [{ height: "0vh" }, { height: "300vh" }];
+    triggerContainer.current.style.display = "flex";
 
-    const transEndEventName = transEndEventNames["transition"];
-    const support = { transitions: true };
+    var animation = triggerContainer.current.animate(keyFrames, {
+      duration: 500,
+      playbackRate: 1,
+      iterations: 1,
+    });
+    var secondAnimation;
 
-    console.log(steps);
-    function toggleOverlay() {
-      if (overlayClass === "open") {
-      } else if (!overlayClass == "close") {
-        var pos = 0;
-        setOverlayClass("open");
+    setTimeout(() => {
+      secondAnimation = triggerContainer.current.animate(
+        [{ width: "10px" }, { width: "400vh" }],
+        {
+          duration: 500,
+          playbackRate: 1,
+          iterations: 1,
+        }
+      );
+    }, 160);
+    setTimeout(() => {
+      secondAnimation.pause();
+      animation.pause();
+    }, 460);
+  }
 
-        var nextStep = function (pos) {
-          pos++;
-          if (pos > stepsTotal - 1) return;
-          path.current.animate({ path: steps[pos] }, 60,  "linear", function () {
-            nextStep(pos);
-          });
-        };
-
-        nextStep(pos);
+  function closeOverlay() {
+    var closeAnimation = triggerContainer.current.animate(
+      [{ width: "400vh" }, { width: "0px" }],
+      {
+        duration: 500,
+        playbackRate: 1,
+        iterations: 1,
       }
-    }
-  });
+    );
+    var secondClosingAnimation;
+
+    setTimeout(() => {
+      secondClosingAnimation = triggerContainer.current.animate(
+        [{ height: "300vh" }, { height: "0vh" }],
+        {
+          duration: 500,
+          playbackRate: 1,
+          iterations: 1,
+        }
+      );
+    }, 0);
+    setTimeout(() => {
+      closeAnimation.pause();
+      secondClosingAnimation.pause();
+    }, 470);
+    setTimeout(() => {
+      triggerContainer.current.style.display = "none";
+    }, 495);
+  }
 
   return (
     <>
       <div className="container">
-        <section>
-          <p>
-            <button className="trigger" ref={triggerBttn} type="button">
-              Open Overlay
-            </button>
-          </p>
+        <section className="section">
+          <div className="main-navigation">
+            <h1 onClick={openOverlay} style={{textAlign:"center"}} >Click here</h1>
+          </div>
+
+          <div className="trigger-container" ref={triggerContainer}>
+            <div className="inner-container">
+              <div className="navigation"></div>
+              <hr />
+              <div className="buttons-group">
+                <button className="social-buttons" type="button">
+                  F
+                </button>
+                <button className="social-buttons" type="button">
+                  F
+                </button>
+                <button
+                  className="trigger-close"
+                  onClick={closeOverlay}
+                  type="button"
+                >
+                  Close Overlay
+                </button>
+                <button className="social-buttons" type="button">
+                  F
+                </button>
+                <button className="social-buttons" type="button">
+                  F
+                </button>
+              </div>
+            </div>
+          </div>
         </section>
-      </div>
-      <div
-        className={`overlay overlay-genie ${overlayClass}`}
-        ref={overlay}
-        data-steps="m 701.56545,809.01175 35.16718,0 0,19.68384 -35.16718,0 z;m 698.9986,728.03569 41.23353,0 -3.41953,77.8735 -34.98557,0 z;m 687.08153,513.78234 53.1506,0 C 738.0505,683.9161 737.86917,503.34193 737.27015,806 l -35.90067,0 c -7.82727,-276.34892 -2.06916,-72.79261 -14.28795,-292.21766 z;m 403.87105,257.94772 566.31246,2.93091 C 923.38284,513.78233 738.73561,372.23931 737.27015,806 l -35.90067,0 C 701.32034,404.49318 455.17312,480.07689 403.87105,257.94772 z;M 51.871052,165.94772 1362.1835,168.87863 C 1171.3828,653.78233 738.73561,372.23931 737.27015,806 l -35.90067,0 C 701.32034,404.49318 31.173122,513.78234 51.871052,165.94772 z;m 52,26 1364,4 c -12.8007,666.9037 -273.2644,483.78234 -322.7299,776 l -633.90062,0 C 359.32034,432.49318 -6.6979288,733.83462 52,26 z;m 0,0 1439.999975,0 0,805.99999 -1439.999975,0 z"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="100%"
-          height="100%"
-          viewBox="0 0 1440 806"
-          ref={s}
-          preserveAspectRatio="none"
-        >
-          <path
-            className="overlay-path"
-            ref={path}
-            d="m 701.56545,809.01175 35.16718,0 0,19.68384 -35.16718,0 z"
-          />
-        </svg>
-        <button type="button" className="overlay-close" ref={closeBttn}>
-          Close
-        </button>
-        <nav>
-          <ul>
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>About</a>
-            </li>
-            <li>
-              <a>Work</a>
-            </li>
-            <li>
-              <a>Clients</a>
-            </li>
-            <li>
-              <a>Contact</a>
-            </li>
-          </ul>
-        </nav>
       </div>
     </>
   );
